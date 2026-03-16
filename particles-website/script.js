98d85551d074500a93c0a55cc71ceb1fa8d96046
SRIDHARA P <thesridhar123@gmail.com>
2026-03-16 19:56:22 +0530

    Add files via upload

---
+++ particles-website/script.js
@@ -0,0 +1,47 @@
+particlesJS("particles-js",{
+
+particles:{
+
+number:{
+value:80
+},
+
+size:{
+value:3
+},
+
+move:{
+speed:2
+},
+
+line_linked:{
+enable:true
+},
+
+color:{
+value:"#ffffff"
+}
+
+},
+
+interactivity:{
+
+events:{
+onhover:{
+enable:true,
+mode:"repulse"
+}
+}
+
+}
+
+});
+
+
+function goExplore(){
+window.location.href = "explore.html";
+}
+
+function goHome(){
+window.location.href = "index.html";
+}
